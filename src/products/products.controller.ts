5c03b40148e7d861ddef787d8cfbf87782694182
Carla Serrano <carlacarly8@gmail.com>
2022-07-06 19:09:48 -0300

    feat: TP added

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -21,69 +21,69 @@ interface IUser {
   name: string;
   description: string;
 }
+
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Get()
-  getHelloInProducts(): Product[] {
+  getAllProducts(): Product[] {
     return this.productsService.getAll();
   }
 
-  @Get('hot')
-  getSpecialProducts(): string {
-    return 'Te vamos a mostrar los productos más calientes!!';
-  }
+  // @Get('hot')
+  // getSpecialProducts(): string {
+  //   return 'Te vamos a mostrar los productos más calientes!!';
+  // }
 
   @Get(':id')
-  find(@Param('id') id: string) {
-    return `Estás consultando el producto ${id}`;
+  find(@Param('id') id: number) {
+    return this.productsService.getId(id);
   }
 
-  @Get(':id/:size')
-  findWithSize(@Param('id') id: number, @Param('size') size: string) {
-    return `En esta ruta obtenemos el producto ${id}, pero en su tamaño ${size}`;
-  }
+  // @Get(':id/:size')
+  // findWithSize(@Param('id') id: number, @Param('size') size: string) {
+  //   return `En esta ruta obtenemos el producto ${id}, pero en su tamaño ${size}`;
+  // }
 
   @Post()
-  @HttpCode(HttpStatus.CONFLICT)
-  createProduct(@Body() body: IUser) {
-    return `Creo un producto ${body.name} con descripción ${body.description}`;
+  @HttpCode(HttpStatus.NO_CONTENT)
+  createProduct(@Body() body) {
+    this.productsService.insert(body);
   }
 
-  @Get('milanesa')
-  rutaQuery(@Query() milanesa: string) {
-    return milanesa;
-  }
+  // @Get('milanesa')
+  // rutaQuery(@Query() milanesa: string) {
+  //   return milanesa;
+  // }
 
-  @Get('cars')
-  carsQuery(@Query('count', ParseIntPipe) carCount: number) {
-    return carCount;
-  }
+  // @Get('cars')
+  // carsQuery(@Query('count', ParseIntPipe) carCount: number) {
+  //   return carCount;
+  // }
 
-  @Get(':id')
-  findOne(@Res() response: Response, @Param('id') id: number) {
-    if (id < 100) {
-      return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
-    } else {
-      return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
-    }
-  }
+  // @Get(':id')
+  // findOne(@Res() response: Response, @Param('id') id: number) {
+  //   if (id < 100) {
+  //     return response.status(HttpStatus.OK).send(`Página del producto ${id}`);
+  //   } else {
+  //     return response.status(HttpStatus.NOT_FOUND).send(`Producto inexistente`);
+  //   }
+  // }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() body) {
-    return `Estás haciendo una operación de actualización del recurso ${id} 
-          con ${body.name} y ${body.description}`;
+    return this.productsService.update(id, body);
   }
 
-  @Patch(':id')
-  partialUpdate(@Param('id') id: number, @Body() body) {
-    return `Actualización parcial del ítem ${id}`;
-  }
+  // @Patch(':id')
+  // partialUpdate(@Param('id') id: number, @Body() body) {
+  //   return `Actualización parcial del ítem ${id}`;
+  // }
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
   delete(@Param('id') id: number) {
-    return `Hemos borrado el producto ${id}`;
+    this.productsService.delete(id);
   }
 }
